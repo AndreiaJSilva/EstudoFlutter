fcaf5d2303f20186be2d56e44d29b4f27980b11b
Andreia Silva <andreia.silva@estudante.ufla.br>
2023-05-02 13:58:52 -0500

    Alterando projeto por coponentes

---
+++ flutter_application_2/lib/myApp.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'homePage.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext) {
+    return const MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
